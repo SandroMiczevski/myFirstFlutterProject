83a9fbb722ca277d6f4df6cea74e1cb1f5eb0762
Sandro A. Miczevski Jr <sandro.miczevski@outlook.com>
2021-06-20 15:07:17 -0300

    First adjustments made to the project structure

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'src/models/appdata.dart';
+
+// Importing views
+import 'src/pages/homepage.dart';
+
+/* The main file will be responsible for routing */
+
+
+void main () => runApp(
+  MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => AppData()),
+    ],
+    child: const MyApp(),
+  )
+);
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/': (context) => HomePage(),
+      },
+      initialRoute: '/',
+    );
+  }
+}
