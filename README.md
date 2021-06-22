1f779ad3562bea1cb25d56477b75b7ae4b0d50e2
Sandro A. Miczevski Jr <sandro.miczevski@outlook.com>
2021-06-21 22:42:18 -0300

    First changes. Splash and Login pages created

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This project was created, throughout a week, during a study session called Next Level Week (NLW).
 
-The app was created with the intetion to be a tool that helps people to manage their payments.
+The app was created with the intention to be a tool that helps people to manage their payments. It was designed to work with a brazilian form of payment, called 'Boleto', simillar to a Bank Slip´.
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,27 @@
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'src/models/appdata.dart';
 
-// Importing views
-import 'src/pages/homepage.dart';
+import 'modules/home/home_page.dart';
 
-/* The main file will be responsible for routing */
+void main(){
+  runApp(const AppWidget());
+}
 
-
-void main () => runApp(
-  MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (context) => AppData()),
-    ],
-    child: const MyApp(),
-  )
-);
-
-class MyApp extends StatelessWidget{
-  const MyApp({Key? key}) : super(key: key);
+class AppWidget extends StatelessWidget {
+  const AppWidget({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        '/': (context) => const HomePage(),
-      },
-      initialRoute: '/',
+      title: "payflow",
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      //home: const SplashPage(),
+      home: const LoginPage(),
+      //home: const HomePage(),
     );
   }
 }
